2ab36ad588cef8aabaf34b52509a4d578162c988
Akis Idrizi <aidrizi@outlook.com.gr>
2023-10-21 15:11:41 +0300

    Add Youtube video handler

--- script.js
+++ script.js
@@ -8,28 +8,52 @@ let wasRemoved = false;
  * @return {boolean} Returns true if any popup element was removed, otherwise false.
  */
 function removePopupElements() {
-  let removed = false;
+	let removed = false;
 
-  POPUP_IDENTIFIERS.forEach((identifier) => {
-    const element = document.querySelector(identifier);
+	POPUP_IDENTIFIERS.forEach((identifier) => {
+		const element = document.querySelector(identifier);
 
-    if (element) {
-      element.remove();
-      removed = true;
-    }
-  });
+		if (element) {
+			element.remove();
+			removed = true;
+		}
+	});
 
-  return removed;
+	return removed;
+}
+
+/**
+ * Promise handler for YouTube Video.
+ *
+ * @param video
+ * @returns {Promise<unknown>}
+ */
+function playVideo(video) {
+	return new Promise((resolve, reject) => {
+		video.play()
+			.then(() => {
+				resolve('Video resuming...');
+			})
+			.catch(error => {
+				reject(`Error occurred while playing the video: ${error}`);
+			});
+	});
 }
 
 /**
  * Resumes the playback of the current video.
  */
 function resumeVideo() {
-  const video = document.querySelector("video.html5-main-video");
-  if (video && video.paused && video.currentTime > 0.0) {
-    video.play();
-  }
+	const video = document.querySelector("video.html5-main-video");
+	if (video?.paused && video.currentTime > 0.0) {
+		playVideo(video)
+			.then(message => {
+				console.log(message);
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
 }
 
 /**
@@ -39,15 +63,15 @@ function resumeVideo() {
  * Stops execution once the popup is removed.
  */
 const observerCallback = () => {
-  if (wasRemoved) return;
+	if (wasRemoved) return;
 
-  wasRemoved = removePopupElements();
-  resumeVideo();
+	wasRemoved = removePopupElements();
+	resumeVideo();
 };
 
 const observer = new MutationObserver(observerCallback);
 
 // Setup observer on the DOM
 if (window.location.hostname === "www.youtube.com") {
-  observer.observe(document.body, { childList: true, subtree: true });
+	observer.observe(document.body, {childList: true, subtree: true});
 }
